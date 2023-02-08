04d4a35ff451f3d370c4a6da5b41637e551a0834
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-02-08 19:00:07 +0300

    MySweater version 1

---
+++ src/main/java/com/example/springboot_sweater/SpringbootSweaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSweaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSweaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_sweater/SpringbootSweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSweaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
